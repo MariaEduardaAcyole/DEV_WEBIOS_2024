0ef32f38da0c0cce53a0ece11561ee85da5b2387
MariaEduardaAcyole <dudaacyole12@gmail.com>
2024-10-10 20:28:03 -0300

    aula 01 js

---
+++ JS/JS01 - Introdução/script.js
@@ -0,0 +1,18 @@
+console.log('hello world denovo Lindo')
+
+// Hello World em JAVA
+// class test {
+//     public static void main (args String[]){
+//         System.out.printIn('hello world');
+//     };
+// }
+[[[[[[[]]]]]]]
+
+// 2 , 3.5 = numero
+
+// const botao = document.getElementById('clique');
+// function clique(botao){
+   // alert('foi');
+//}
+
+alert('hello world querido')
