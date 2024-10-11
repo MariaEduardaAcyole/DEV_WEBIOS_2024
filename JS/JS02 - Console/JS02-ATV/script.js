44055b40d774ced95fad878c0f7e27ae34b5746d
MariaEduardaAcyole <dudaacyole12@gmail.com>
2024-10-10 21:42:49 -0300

    aula 02 js

---
+++ JS/JS02 - Console/JS02-ATV/script.js
@@ -0,0 +1,13 @@
+// Crie um arquivo main.js e crie um programa que 
+// leia quatro notas  de um aluno 
+// e tire a média das notas no console.log 
+// apresente a seguinte mensagem "A média do aluno com as 4 notas é:"
+
+let nota1 = 9
+let nota2 = 7
+let nota3 = 10
+let nota4 = 8
+
+let media = (nota1+nota2+nota3+nota4)/4
+
+console.log("A média do aluno com as 4 notas é:", media)
---
+++ JS/JS02 - Console/script.js
@@ -0,0 +1,30 @@
+//nome de variaveis (não começa com numero, caractere especial)
+// = atribuição == comparação === veriicar se é identico
+// let - nao permite repetir o nome da variavel
+// const - nao pode alterar ela
+
+
+//Numeros
+// infinity , -infinity = os numeros infinitos
+// boleanos - true ou false
+
+var nome = 0 // numerico
+var nome = 'Homer' //string
+var teste // não iniciada
+
+
+let nomeAluno = 'duda'
+
+nomeAluno = 'jojo'
+console.log('primeiro:', nomeAluno);
+
+nomeAluno ="joaozinho"
+console.log(`depois: ${nomeAluno}`); //
+
+var teste = 0/0
+console.log(teste)
+
+var texto = 'lorem'
+
+var verdadeira = true
+var falsa = false
