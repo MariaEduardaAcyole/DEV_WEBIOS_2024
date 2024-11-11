a97394210f0f810fcfd90157c619fcd2b9216a37
MariaEduardaAcyole <dudaacyole12@gmail.com>
2024-11-11 19:43:00 -0300

    atv 1 e 2 aula 10

---
+++ JS/JS10-metodoArray/atv10/atv1.js
@@ -0,0 +1,18 @@
+// Exercício 1
+// Crie um Array unidimensional com 8 posições contendo os seguintes valores 17, 43, 8, 4, 97, 56, 29, 95 em cada posição irá conter um valor do tipo number.
+
+let valores = [5, 8, 9, 3, 10, 58, 14, 20]
+
+// utilizando o método forEach crie uma função que receba cada valor do array e verifique se o número é par ou ímpar e exiba o resultado no console.log
+
+function ImparPar (valor){
+
+    if (valor % 2 == 0) {
+        console.log(valor, "par")
+    }
+    else{
+        console.log(valor, "impar")
+
+    }
+}
+
